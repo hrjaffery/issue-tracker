ab4d76f3feef5e911b003275ea9afe819324cd4b
hrjaffery <hasnainrazajaffery13@gmail.com>
2025-02-07 18:32:43 +0500

    Add NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { IoBugSharp } from "react-icons/io5";
-
+import classNames from "classnames";
 const NavBar = () => {
+  const pathName = usePathname();
+  console.log("🚀 ~ NavBar ~ pathName:", pathName);
+
   const links = [
     { label: "Dashboard", href: "/dashboard" },
     { label: "Issues", href: "/issues" },
@@ -15,7 +20,11 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li
-            className=" text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": pathName === link.href,
+              "text-zinc-500": pathName !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             key={link.href}
           >
             <Link href={link.href}>{link.label}</Link>
