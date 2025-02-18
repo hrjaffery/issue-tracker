975535f8f6c929e40f0debb948d4634886d611f0
hrjaffery <hasnainrazajaffery13@gmail.com>
2025-02-18 15:16:45 +0500

    Add custom error messages

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const issueSchema = z.object({
-  title: z.string().min(1).max(244),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required.").max(244),
+  description: z.string().min(1, "Description is required."),
 });
